84e1aa1f2d5000dd31a93a41eb07b6852235d779
jdp@wtkx.org <jdp@wtkx.org@63f8a6b4-cb2e-11de-a8df-5d31f88aefc0>
2010-01-05 07:00:32 +0000

    upd

--- web/mimetypes.sh
+++ web/mimetypes.sh
@@ -1,3 +1,18 @@
-svn ps svn:mime-type application/x-java-vm *.jar
-svn ps svn:mime-type application/x-java-jnlp-file *.jnlp
-svn ps svn:mime-type text/plain *.txt *.sh
+#!/bin/bash
+
+function mimetype {
+ T=$1
+ shift
+ F=$*
+ if [ -n "${T}" ]&&[ -n "${F}" ]
+ then
+  svn ps svn:mime-type ${T} ${F}
+ fi
+}
+
+mimetype application/x-java-archive $(find . -type f -name "*.jar")
+mimetype application/x-java-jnlp-file $(find . -type f -name "*.jnlp")
+mimetype text/plain $(find . -type f -name "*.txt" -o -name "*.sh")
+mimetype application/x-3ds $(find . -type f -name "*.3ds")
+mimetype image/png $(find . -type f -name "*.png")
+mimetype image/jpeg $(find . -type f -name "*.jpg" -o -name "*.jpeg")
